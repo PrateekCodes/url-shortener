f51024b67076052678ec0dbce032cd87b1def2f9
Prateek Arora <prateekisthebest98@gmail.com>
2020-05-13 21:07:36 +0530

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_13_152409) do
+ActiveRecord::Schema.define(version: 2020_05_13_153543) do
 
   create_table "urls", force: :cascade do |t|
-    t.string "url"
-    t.string "short_url"
+    t.string "url", null: false
+    t.string "short_url", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
