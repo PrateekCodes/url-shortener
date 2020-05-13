7a465bcdaac190cfc6a00bfd8177086b0b04fabf
Prateek Arora <prateekisthebest98@gmail.com>
2020-05-13 23:39:35 +0530

    add url controller

---
+++ app/controllers/urls_controller.rb
@@ -0,0 +1,27 @@
+class UrlsController < ApplicationController
+
+  def create
+    @url = Url.new(url_params)
+    @url.generate_short_url
+    if @url.save
+      render status: :created, json: { success: true, short_url: @url.short_url}
+    else
+      render status: :unprocessable_entity, json: { success: false, errors: @url.errors.full_messages }
+    end
+  end
+
+  def show
+    @url = Url.find_by_short_url(params[:short_url])
+
+    if @url
+      render status: :ok, json: { success: true, url: @url.original_url }
+    else
+      render status: :not_found, json: { success: false, error: "Not found" }
+    end
+  end
+
+  private
+    def url_params
+      params.require(:url).permit(:original_url)
+    end
+end
